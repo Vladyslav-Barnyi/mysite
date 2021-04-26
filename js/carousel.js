685fdd59a3702e73863a811f70dfadd8a01b5d17
vadym-lviv <vadym.kosteckiy@gmail.com>
2021-04-26 13:38:43 +0000

    Sample Code

---
+++ js/carousel.js
@@ -0,0 +1,22 @@
+const image = [
+    "../img/logo1.png",
+    "../img/logo2.png",
+    "../img/logo3.png",
+    "../img/logo4.png",
+    "../img/logo5.png",
+    "../img/logo6.png",
+    "../img/logo7.png"
+]
+let currentSlide = 0;
+
+function showSlide() {
+    const carouselImage = document.querySelector('.products-carousel img');
+    carouselImage.src = images[currentSlide];
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= images.length) currentSlide = 0;
+    showSlide();
+}
+setInterval (nextSlide, 1000);
